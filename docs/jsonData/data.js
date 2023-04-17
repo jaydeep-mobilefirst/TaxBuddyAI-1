2f21ea210339b1c10f886b98b6e68351cf2d55e5
Jaydeep Patel <58411819+jaydeep-patel-au6@users.noreply.github.com>
2023-04-17 18:56:35 +0530

    create data.js file to input jsondata

---
+++ docs/jsonData/data.js
@@ -0,0 +1,17 @@
+export const jsonData = [];
+
+// put your josn data in to jsonData array 
+// Example jsonData = [
+//   {
+//     key: value,
+//     key: value,
+//     key: value,
+//   },
+//   {
+//     key: value,
+//   },
+//   {
+//     key: value,
+//     key: value,
+//   },
+// ];
